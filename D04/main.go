8687fdd52b09c98cc1f737f450ca466e85afe3e3
Prakita Renatin <prakita.renatin@ororatech.com>
2023-12-04 19:24:58 +0100

    D04 part 2

--- D04/main.go
+++ D04/main.go
@@ -106,9 +106,28 @@ func iterateLines(filePath string, callback func(string) error) error {
     return nil
 }
 
+// Returns a new countArr
+func applyWin(index int, matchArr []int, countArr []int) []int {
+    n := len(matchArr)
+    if index >= n - 1 {
+        return countArr
+    } else if matchArr[index] == 0 {
+        return countArr
+    } else {
+        for i := index + 1; i < index + matchArr[index] + 1; i++ {
+            countArr[i] = countArr[i] + 1
+            countArr = applyWin(i, matchArr, countArr)
+        }
+    }
+
+    return countArr
+}
+
 func main() {
     sum := 0
 
+    matchArr := []int{}
+    countArr := []int{}
     callback := func(line string) error {
         // splitting for Game ID part and BallSet part
         v := strings.SplitN(line, ":", 2)
@@ -122,12 +141,27 @@ func main() {
 
         matchingSet := winningSet.Intersect(drawSet)
 
+        matchArr = append(matchArr, len(matchingSet.set))
+        countArr = append(countArr, 1)
         if len(matchingSet.set) > 0 {
             sum += 1 << (len(matchingSet.set) - 1)
         }
         return nil
     }
     err := iterateLines("./input.txt", callback)
+
+    //fmt.Println(matchArr)
+    for i := len(matchArr) - 1; i >= 0; i-- {
+        countArr = applyWin(i, matchArr, countArr)
+        //fmt.Println(countArr)
+    }
+
+    countSum := 0
+    for _, count := range countArr {
+        countSum += count
+    }
+
+    fmt.Printf("Final Count Sum: %d\n", countSum)
     fmt.Printf("Final Winning Point Sum: %d\n", sum)
     fmt.Printf("Error of iterate lines: %v\n", err)
 }
