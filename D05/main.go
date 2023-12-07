53d04dcd18d187264be5f79a8fb7d88c464efd76
Prakita Renatin <prakita.renatin@ororatech.com>
2023-12-07 01:20:44 +0100

    calling it a day for now with D05

--- D05/main.go
+++ D05/main.go
@@ -13,7 +13,7 @@ import (
 
 type Range struct {
     start int // inclusive start
-    end int // exclusive end
+    n int // length of range
 }
 
 type MapRange struct {
@@ -44,8 +44,8 @@ func FindNextLowest(ref int, ranges []MapRange) (MapRange, error) {
     }
 }
 
-// Mapping ranges
-func MapsTo(in int, ranges []MapRange) int {
+// Mapping ranges, assumes that ranges is already sorted
+func MapsToSingle(in int, ranges []MapRange) int {
     nextLowestRange, err := FindNextLowest(in, ranges)
     //fmt.Printf("\t\tfoudn next lowest: %v\n", nextLowestRange)
     if err != nil {
@@ -55,6 +55,65 @@ func MapsTo(in int, ranges []MapRange) int {
     } else {
         return nextLowestRange.dst + (in - nextLowestRange.src)
     }
+}
+
+// TODO
+// Mapping ranges, assumes that mapRanges is already sorted
+func MapsToRange(in Range, mapRanges []MapRange) []Range {
+    retRanges := []Range{}
+    idxLastRange := len(mapRanges) - 1
+    idxFirstRange := 0
+
+    findMapRange := func(mr MapRange) int {
+        for i, v := range mapRanges {
+            if v == mr {
+                return i
+            }
+        }
+        return -1
+    }
+
+    nextLowestOfEnd, err1 := FindNextLowest(in.start + in.n - 1, mapRanges)
+    if err1 != nil {
+        // The entire range is outside of the map ranges
+        return append(retRanges, in)
+    } else {
+        // Setting the idx of the last overlapping range
+        idxLastRange = findMapRange(nextLowestOfEnd)
+    }
+
+    nextLowestOfStart, err2 := FindNextLowest(in.start, mapRanges)
+    if err2 == nil {
+        // If there is a next lowest
+        idxFirstRange = findMapRange(nextLowestOfStart)
+    }
+
+    for idx:=idxFirstRange; idx<idxLastRange+1; idx++ {
+        out := Range{
+            start: mapRanges[idx].dst,
+            n: mapRanges[idx].n,
+        }
+        // Order of these if statements is important
+
+        // Special Handling, input range start after map range starts
+        if idx == idxFirstRange && err2 != nil {
+            if in.start > nextLowestOfStart.src {
+                out.start += in.start - nextLowestOfStart.src
+                out.n -= in.start - nextLowestOfStart.src
+            }
+        }
+
+        // Special Handling, if input range stops before map range stops
+        if idx == idxLastRange {
+            if in.start + in.n < mapRanges[idx].src + mapRanges[idx].n  {
+                // Subtracting the difference
+                out.n -= (mapRanges[idx].src + mapRanges[idx].n) - (in.start + in.n)
+            }
+        }
+
+        retRanges = append(retRanges, out)
+    }
+    return retRanges
 }
 
 func parseInts(s string) ([]int, error) {
@@ -120,7 +179,7 @@ func main() {
         fmt.Printf("seed: %d\n", seed)
         for i:=0; i<TOTAL_MAPS; i++ {
             //fmt.Printf("\tranges: %v\n", rangesList[i])
-            in = MapsTo(in, rangesList[i])
+            in = MapsToSingle(in, rangesList[i])
             //fmt.Printf("\tin: %d\n", in)
         }
         fmt.Printf("\tfinal: %d\n", in)
