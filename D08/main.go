7f2b1e3e89e3707354348092725701612048b860
Prakita Renatin <prakita.renatin@ororatech.com>
2023-12-12 13:44:22 +0100

    WIP Day 08

--- D08/main.go
+++ D08/main.go
@@ -1,15 +1,24 @@
 package main
 
 import (
+	"crypto/md5"
 	"errors"
 	"fmt"
 	"regexp"
+	"time"
 
 	"github.com/Rakagami/goaoc2023/utils"
 )
 
 type Element string
 type ElementSet map[Element]bool
+
+type InstructionElementHash [16]byte
+// Returns a canonical Md5 hash of element set and move
+func (es Element) Hash(instruction string) InstructionElementHash {
+    hash := md5.Sum([]byte(string(es) + instruction))
+    return hash
+}
 
 func (es ElementSet) Add(e Element) ElementSet {
     es[e] = true
@@ -21,9 +30,22 @@ func (es ElementSet) Remove(e Element) ElementSet {
     return es
 }
 
-func (es ElementSet) Terminal() bool {
-    _, ok := es["ZZZ"]
-    return ok
+func (es ElementSet) Union(other ElementSet) ElementSet {
+    for e, _ := range other {
+        es[e] = true
+    }
+    return es
+}
+
+// Checkes whether element set is in final state
+func (es ElementSet) IsFinal(finalSet ElementSet) bool {
+    for e := range finalSet {
+        _, ok := es[e]
+        if !ok {
+            return false
+        }
+    }
+    return true
 }
 
 type Rule struct {
@@ -58,8 +80,56 @@ func (rs RuleSet) Move(left bool, e Element) Element {
     }
 }
 
-//func (rs RuleSet) Move(left bool, es ElementSet) ElementSet {
-//}
+func (rs RuleSet) MoveInstruction(instructionString string, e Element) Element {
+    for _, c := range instructionString {
+        switch(c) {
+        case 'L':
+            e = rs.Move(true, e)
+            break
+        case 'R':
+            e = rs.Move(false, e)
+            break
+        default:
+            panic(errors.New("Unexpected state reached"))
+        }
+    }
+    return e
+}
+
+func (rs RuleSet) MoveSet(left bool, es ElementSet) ElementSet {
+    retEs := ElementSet{}
+    for e := range es {
+        retEs = retEs.Add(rs.Move(left, e))
+    }
+    return retEs
+}
+
+func (rs RuleSet) MoveSetInstruction(instructionString string, es ElementSet, lookupTable map[InstructionElementHash]Element) (ElementSet, map[InstructionElementHash]Element) {
+    returnSet := ElementSet{}
+    for e := range es {
+        lookupUpElement := rs.MoveInstruction(instructionString, e)
+        returnSet = returnSet.Add(lookupUpElement)
+        lookupTable[e.Hash(instructionString)] = lookupUpElement
+    }
+    return returnSet, lookupTable
+}
+
+// Returns starting and ending ElementSets
+func (rs RuleSet) GetTerminalElements() (ElementSet, ElementSet) {
+    starting := ElementSet{}
+    ending := ElementSet{}
+
+    for el := range rs.ruleMap {
+        c := el[2]
+        if c == 'A' {
+            starting = starting.Add(el)
+        } else if c == 'Z' {
+            ending = ending.Add(el)
+        }
+    }
+
+    return starting, ending
+}
 
 // Parses the string into a rule
 func parseRule(s string) Rule {
@@ -95,31 +165,48 @@ func main() {
         return nil
     }
 
-    utils.IterateLines("input.txt", parseLines)
+    utils.IterateLines("input_test2.txt", parseLines)
  
     // ============ Logic ============
 
-    fmt.Printf("RuleSet %v\n", ruleSet)
+    //fmt.Printf("RuleSet %v\n", ruleSet)
     fmt.Printf("baseInstruction %v\n", baseInstruction)
 
     // Current Element
-    cur := Element("AAA")
-    cnt := 0
-    for cur != "ZZZ" {
-        instruction := baseInstruction[cnt % len(baseInstruction)]
-        switch(instruction) {
-        case 'L':
-            cur = ruleSet.Move(true, cur)
-            cnt++
-            break
-        case 'R':
-            cur = ruleSet.Move(false, cur)
-            cnt++
-            break
-        default:
-            panic(errors.New("Unexpected state reached"))
+    cur, finalSet := ruleSet.GetTerminalElements()
+
+    fmt.Printf("StartingSet: %v\n", cur)
+    fmt.Printf("FinalSet: %v\n", finalSet)
+    iterationCnt := 0
+    lookupTable := map[InstructionElementHash]Element{}
+    for !cur.IsFinal(finalSet) {
+        // Check which elements can be found in the lookup table
+        lookedUpElements := ElementSet{}
+        restElements := ElementSet{}
+        for c := range cur {
+            hash := c.Hash(baseInstruction)
+            _, ok := lookupTable[hash]
+            if ok {
+                fmt.Printf("\tUsed Lookup table\n")
+                lookedUpElements = lookedUpElements.Add(c)
+            } else {
+                restElements = restElements.Add(c)
+            }
         }
+
+        // If there are some unlooked up elements, iterate through
+        if len(restElements) > 0 {
+            var calculatedElements ElementSet
+            calculatedElements, lookupTable = ruleSet.MoveSetInstruction(baseInstruction, restElements, lookupTable)
+            lookedUpElements = lookedUpElements.Union(calculatedElements)
+        }
+
+        cur = lookedUpElements
+        iterationCnt++
+
+        fmt.Printf("cur: %v\n", cur)
+        time.Sleep(100 * time.Millisecond)
     }
 
-    fmt.Printf("Counter: %v\n", cnt)
+    fmt.Printf("Counter: %v\n", iterationCnt * len(baseInstruction))
 }
